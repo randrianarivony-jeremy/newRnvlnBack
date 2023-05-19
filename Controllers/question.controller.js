3008ef73a0badf4fd7bee70ad8617e30496ea9ef
Koloina <koloinaimaginieur@gmail.com>
2023-05-19 23:13:18 +0300

    mutliple questions

--- Controllers/question.controller.js
+++ Controllers/question.controller.js
@@ -16,7 +16,7 @@ module.exports.createQuestion = async(req,res)=>{
 module.exports.readQuestion = async(req,res)=>{
     try {
         const result = await questionModel.findById(req.params.id)
-        .populate('interviewer','name picture')
+        .populate('interviewer','name picture job')
         res.status(200).json(result)
     } catch (error) {
         res.status(500).send(error.message);
