3ed3ad11ea0a069011e5d1e45f16cb0a331ee206
Koloina <koloinaimaginieur@gmail.com>
2023-06-15 13:35:43 +0300

    stable

--- Models/publication.model.js
+++ Models/publication.model.js
@@ -6,41 +6,38 @@ const publicationSchema = new mongoose.Schema(
     id_user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "user",
-      required:true
+      required: true,
     },
     type: {
-        type:String,
-        default:'article'
+      type: String,
+      default: "publication",
     },
     data: [
       {
         content: { type: String, required: true, trim: true },
-        contentType: { type: String, required: true},
+        contentType: { type: String, required: true },
         bg: { type: String, default: "" },
         description: { type: String, default: "", trim: true },
       },
     ],
-    public: {type:Boolean,default:false},
+    public: { type: Boolean, default: false },
     likers: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "user",
       },
     ],
-    likeNotification: 
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "notification",
-        default:null
-      }
-    ,
+    likeNotification: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "notification",
+      default: null,
+    },
     comments: [CommentSchema],
-    commentNotification: 
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "notification",
-        default:null
-      }
+    commentNotification: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "notification",
+      default: null,
+    },
   },
   {
     timestamps: true,
